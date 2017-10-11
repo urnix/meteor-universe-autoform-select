efcec60ae2fe3f5b38502836e8e1b694ca4ac060
Artem <2urnix@gmail.com>
2017-10-11 17:50:13 +0300

    fix(.meteor/package.js): update `aldeed:autoform` version

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     name: 'artemi:universe-autoform-select',
     summary: 'Custom "afUniverseSelect" input type for AutoForm, with the appearance as selectize',
-    version: '0.3.10',
+    version: '0.3.11',
     git: 'https://github.com/urnix/meteor-universe-autoform-select.git'
 });
 
@@ -17,7 +17,7 @@ Package.onUse(function (api) {
     }
 
     api.use(['ecmascript', 'templating', 'underscore'], 'client');
-    api.use('aldeed:autoform@5.8.1');
+    api.use('aldeed:autoform@6.2.0');
     api.use('artemi:universe-selectize@0.1.23', 'client');
 
     api.addFiles([
